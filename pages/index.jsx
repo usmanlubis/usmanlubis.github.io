23773d1a1fa9f077dd6b5e60bade1d5deb8303f0
usmanlubis <usmanlubis11@gmail.com>
2023-02-14 18:28:10 +0700

    test deploy

--- pages/index.jsx
+++ pages/index.jsx
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1 className="text-3xl font-bold underline">
-        Hello world!
+        Hello world!!!
       </h1>
     </>
   );
