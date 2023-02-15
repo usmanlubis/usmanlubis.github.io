1c5f10d6e935d5bbe00384b20a153e0daac4c0c4
usmanlubis <usmanlubis11@gmail.com>
2023-02-15 17:37:47 +0700

    add hamburger menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,32 +1,40 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { AiOutlineMenu } from 'react-icons/ai';
 
 export default function Navbar() {
   return (
-    <div className="fixed w-full h-20 shadow-xl z=[100]">
+    <div className="fixed w-full h-20 shadow-xl z-1">
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
         <Image src="/../public/assets/NavbarLogo.png" alt="Navbar Logo" width={50} height={50} />
 
         <div>
           <ul className="hidden md:flex">
             <Link href="/">
-              <li className="ml-10 text-sm uppercase hover:opacity-80">Home</li>
+              <li className="ml-10 text-sm uppercase hover:opacity-70">Home</li>
             </Link>
             <Link href="/">
-              <li className="ml-10 text-sm uppercase hover:opacity-80">About</li>
+              <li className="ml-10 text-sm uppercase hover:opacity-70">About</li>
             </Link>
             <Link href="/">
-              <li className="ml-10 text-sm uppercase hover:opacity-80">Skills</li>
+              <li className="ml-10 text-sm uppercase hover:opacity-70">Skills</li>
             </Link>
             <Link href="/">
-              <li className="ml-10 text-sm uppercase hover:opacity-80">Projects</li>
+              <li className="ml-10 text-sm uppercase hover:opacity-70">Projects</li>
             </Link>
             <Link href="/">
-              <li className="ml-10 text-sm uppercase hover:opacity-80">Contact</li>
+              <li className="ml-10 text-sm uppercase hover:opacity-70">Contact</li>
             </Link>
           </ul>
+          <div className="md:hidden">
+            <AiOutlineMenu size={30} className="cursor-pointer hover:opacity-70" />
+          </div>
         </div>
+
+        {/* <div className="">
+
+        </div> */}
       </div>
     </div>
   );
