1fe5f60a17178326cf9f52b8e90bcc41e11dbe31
usmanlubis <usmanlubis11@gmail.com>
2023-02-15 15:59:08 +0700

    clean up files and code

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 // import Image from 'next/image';
-// import { Inter } from '@next/font/google';
-// import Main from '@/components/Main';
 import Navbar from '../components/Navbar';
-
-// const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
@@ -14,7 +10,7 @@ export default function Home() {
         <title>Usman Lubis</title>
         <meta name="description" content="I’m a front-end web developer specializing." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/Favicon.png" />
+        <link rel="icon" href="/assets/Favicon.png" />
       </Head>
       <Navbar />
     </>
