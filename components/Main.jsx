5dd5719e4b166987c80e78543e638889260d00d3
usmanlubis <usmanlubis11@gmail.com>
2023-02-16 15:58:23 +0700

    add main section

---
+++ components/Main.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Link from 'next/link';
+import { AiFillGithub, AiFillMail } from 'react-icons/ai';
+import { FaLinkedinIn, FaTwitter } from 'react-icons/fa';
+
+function Main() {
+  return (
+    <main className="w-full h-screen pt-20">
+      <div className="w-full max-w-[1240px] h-full text-center mx-auto px-8 flex justify-center items-center">
+        <div>
+          <p className="uppercase tracking-widest my-2">Let&apos;s build something cool together</p>
+          <h1>
+            Hi, I am
+            <span className="text-[#38BDF8]"> Usman</span>
+          </h1>
+          <h1>A Front-End Web Developer</h1>
+          <p className="max-w-xl m-auto my-4">
+            I&apos;m a front-end web developer specializing in building. Currently,
+            I&apos;m focused on building responsive front-end web application
+            while learning back-end technologies
+          </p>
+          <div href="/" className="flex justify-between items-center w-[80%] mx-auto mt-8">
+            <Link href="/" className="hover:scale-125 ease-in duration-100 rounded-full shadow-xl shadow-gray-500 p-4 md:p-5 md:shadow-2xl md:shadow-gray-500">
+              <FaLinkedinIn size={20} />
+            </Link>
+            <Link href="/" className="hover:scale-125 ease-in duration-100 rounded-full shadow-xl shadow-gray-500 p-4 md:p-5 md:shadow-2xl md:shadow-gray-500">
+              <AiFillGithub size={20} />
+            </Link>
+            <Link href="/" className="hover:scale-125 ease-in duration-100 rounded-full shadow-xl shadow-gray-500 p-4 md:p-5 md:shadow-2xl md:shadow-gray-500">
+              <AiFillMail size={20} />
+            </Link>
+            <Link href="/" className="hover:scale-125 ease-in duration-100 rounded-full shadow-xl shadow-gray-500 p-4 md:p-5 md:shadow-2xl md:shadow-gray-500">
+              <FaTwitter size={20} />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default Main;
