53601c3886aae2b3dec45276a0c5018ea1a7efa6
usmanlubis <usmanlubis11@gmail.com>
2023-02-17 09:41:59 +0700

    add contact section

---
+++ components/Contact.jsx
@@ -0,0 +1,136 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { AiOutlineMail } from 'react-icons/ai';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import ContactImg from '../public/assets/contact.jpg';
+
+function Contact() {
+  return (
+    <section id="contact" className="w-full">
+      <div className="max-w-[1240px] pt-[5.5rem] m-auto px-8 md:px-16">
+        <h2 className="text-center mb-8">Contact me</h2>
+        <div className="grid lg:grid-cols-5 gap-8">
+          {/* left */}
+          <div className="col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4">
+            <div className="lg:p-4 h-full ">
+              <div>
+                <Image
+                  className="rounded-xl hover:scale-105 ease-in duration-300"
+                  src={ContactImg}
+                  alt="/"
+                />
+              </div>
+              <div>
+                <h2 className="py-2">Usman Lubis</h2>
+                <p>Front-End Developer</p>
+                <p className="py-4">
+                  I am available for freelance or full-time positions. Contact
+                  me and let&apos;s talk.
+                </p>
+              </div>
+              <div>
+                <p className="uppercase pt-8">Connect With Me</p>
+                <div className="flex items-center justify-between py-4">
+                  <a
+                    href="https://www.linkedin.com/in/clint-briley-50056920a/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                      <FaLinkedinIn />
+                    </div>
+                  </a>
+                  <a
+                    href="https://github.com/fireclint"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                      <FaGithub />
+                    </div>
+                  </a>
+
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                    <AiOutlineMail />
+                  </div>
+                  <Link href="/resume">
+                    <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                      <BsFillPersonLinesFill />
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* right */}
+          <div className="col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4">
+            <div className="p-4">
+              <form
+                action="https://getform.io/f/08ebcd37-f5b5-45be-8c13-714f011ce060"
+                method="POST"
+                encType="multipart/form-data"
+              >
+                <div className="grid md:grid-cols-2 gap-4 w-full py-2">
+                  <div className="flex flex-col">
+                    <label htmlFor="name" className="uppercase text-sm py-2">Name</label>
+                    <input
+                      className="border-2 rounded-lg p-3 flex border-gray-300"
+                      type="text"
+                      name="name"
+                      id="name"
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <label htmlFor="phone" className="uppercase text-sm py-2">
+                      Phone Number
+                    </label>
+                    <input
+                      className="border-2 rounded-lg p-3 flex border-gray-300"
+                      type="text"
+                      name="phone"
+                      id="phone"
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-col py-2">
+                  <label htmlFor="email" className="uppercase text-sm py-2">Email</label>
+                  <input
+                    className="border-2 rounded-lg p-3 flex border-gray-300"
+                    type="email"
+                    name="email"
+                    id="email"
+                  />
+                </div>
+                <div className="flex flex-col py-2">
+                  <label htmlFor="subject" className="uppercase text-sm py-2">Subject</label>
+                  <input
+                    className="border-2 rounded-lg p-3 flex border-gray-300"
+                    type="text"
+                    name="subject"
+                    id="subject"
+                  />
+                </div>
+                <div className="flex flex-col py-2">
+                  <label htmlFor="message" className="uppercase text-sm py-2">Message</label>
+                  <textarea
+                    className="border-2 rounded-lg p-3 border-gray-300"
+                    rows="10"
+                    name="message"
+                  />
+                </div>
+                <button type="button" className="button w-full p-4 text-gray-100 mt-4">
+                  Send Message
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Contact;
