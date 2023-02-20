71e8b2ff4410a25aca476b9ceae4df8c5257edbc
usmanlubis <usmanlubis11@gmail.com>
2023-02-20 11:46:55 +0700

    add telegram to main section

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import { AiFillGithub, AiFillMail } from 'react-icons/ai';
-import { FaLinkedinIn, FaTwitter } from 'react-icons/fa';
+import { FaLinkedinIn, FaTelegramPlane } from 'react-icons/fa';
 
 function Main() {
   return (
@@ -20,17 +20,37 @@ function Main() {
             while learning back-end technologies
           </p>
           <div href="/" className="flex justify-between items-center w-[80%] mx-auto mt-8">
-            <Link href="/" className="hover:scale-125 ease-in duration-100 rounded-full shadow-xl shadow-gray-500 p-4 md:p-5 md:shadow-2xl md:shadow-gray-500">
+            <Link
+              href="/"
+              target="_blank"
+              rel="noreferrer"
+              className="hover:scale-125 ease-in duration-100 rounded-full shadow-xl shadow-gray-500 p-4 md:p-5 md:shadow-2xl md:shadow-gray-500"
+            >
               <FaLinkedinIn size={20} />
             </Link>
-            <Link href="/" className="hover:scale-125 ease-in duration-100 rounded-full shadow-xl shadow-gray-500 p-4 md:p-5 md:shadow-2xl md:shadow-gray-500">
+            <Link
+              href="https://www.github.com/usmanlubis"
+              target="_blank"
+              rel="noreferrer"
+              className="hover:scale-125 ease-in duration-100 rounded-full shadow-xl shadow-gray-500 p-4 md:p-5 md:shadow-2xl md:shadow-gray-500"
+            >
               <AiFillGithub size={20} />
             </Link>
-            <Link href="/" className="hover:scale-125 ease-in duration-100 rounded-full shadow-xl shadow-gray-500 p-4 md:p-5 md:shadow-2xl md:shadow-gray-500">
+            <Link
+              href="mailto:usmanlubis11@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+              className="hover:scale-125 ease-in duration-100 rounded-full shadow-xl shadow-gray-500 p-4 md:p-5 md:shadow-2xl md:shadow-gray-500"
+            >
               <AiFillMail size={20} />
             </Link>
-            <Link href="/" className="hover:scale-125 ease-in duration-100 rounded-full shadow-xl shadow-gray-500 p-4 md:p-5 md:shadow-2xl md:shadow-gray-500">
-              <FaTwitter size={20} />
+            <Link
+              href="https://t.me/uslubis"
+              target="_blank"
+              rel="noreferrer"
+              className="hover:scale-125 ease-in duration-100 rounded-full shadow-xl shadow-gray-500 p-4 md:p-5 md:shadow-2xl md:shadow-gray-500"
+            >
+              <FaTelegramPlane size={20} />
             </Link>
           </div>
         </div>
