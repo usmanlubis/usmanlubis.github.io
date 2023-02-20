a8fc7875ce430d9da8dc4b281e7587d88cfb3e40
usmanlubis <usmanlubis11@gmail.com>
2023-02-20 20:03:06 +0700

    change src image method

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import Image from 'next/image';
 import HtmlLogo from '../public/assets/skills/html.png';
+import CssLogo from '../public/assets/skills/css.png';
+import JavascriptLogo from '../public/assets/skills/javascript.png';
+import ReactJSLogo from '../public/assets/skills/react.png';
+import TailwindLogo from '../public/assets/skills/tailwind.png';
+import NextJSLogo from '../public/assets/skills/nextjs.png';
+import GitLogo from '../public/assets/skills/git.png';
+import NodeJSLogo from '../public/assets/skills/node.png';
+// import ExpressJSLogo from '../public/assets/skills/express.png';
 
 function Skills() {
   return (
@@ -14,37 +22,37 @@ function Skills() {
               <p>HTML</p>
             </div>
             <div className="flex justify-between items-center p-4 shadow-lg shadow-gray-500 rounded-lg hover:scale-105 ease-in duration-100">
-              <Image src="/../public/assets/skills/css.png" alt="Css logo" width={50} height={50} />
+              <Image src={CssLogo} alt="Css logo" width={50} height={50} />
               <p>CSS</p>
             </div>
             <div className="flex justify-between items-center p-4 shadow-lg shadow-gray-500 rounded-lg hover:scale-105 ease-in duration-100">
-              <Image src="/../public/assets/skills/javascript.png" alt="JavaScript logo" width={50} height={50} />
+              <Image src={JavascriptLogo} alt="JavaScript logo" width={50} height={50} />
               <p>JavaScript</p>
             </div>
             <div className="flex justify-between items-center p-4 shadow-lg shadow-gray-500 rounded-lg hover:scale-105 ease-in duration-100">
-              <Image src="/../public/assets/skills/react.png" alt="React logo" width={50} height={50} />
+              <Image src={ReactJSLogo} alt="React JS logo" width={50} height={50} />
               <p>ReactJS</p>
             </div>
             <div className="flex justify-between items-center p-4 shadow-lg shadow-gray-500 rounded-lg hover:scale-105 ease-in duration-100">
-              <Image src="/../public/assets/skills/tailwind.png" alt="Tailwind logo" width={50} height={50} />
+              <Image src={TailwindLogo} alt="Tailwind logo" width={50} height={50} />
               <p>Tailwind CSS</p>
             </div>
             <div className="flex justify-between items-center p-4 shadow-lg shadow-gray-500 rounded-lg hover:scale-105 ease-in duration-100">
-              <Image src="/../public/assets/skills/nextjs.png" alt="Next logo" width={50} height={50} />
+              <Image src={NextJSLogo} alt="Next logo" width={50} height={50} />
               <p>NextJS</p>
             </div>
             <div className="flex justify-between items-center p-4 shadow-lg shadow-gray-500 rounded-lg hover:scale-105 ease-in duration-100">
-              <Image src="/../public/assets/skills/git.png" alt="Git logo" width={50} height={50} />
+              <Image src={GitLogo} alt="Git logo" width={50} height={50} />
               <p>Git</p>
             </div>
             <div className="flex justify-between items-center p-4 shadow-lg shadow-gray-500 rounded-lg hover:scale-105 ease-in duration-100">
-              <Image src="/../public/assets/skills/node.png" alt="Node logo" width={50} height={50} />
+              <Image src={NodeJSLogo} alt="Node logo" width={50} height={50} />
               <p>NodeJS</p>
             </div>
-            <div className="flex justify-between items-center p-4 shadow-lg shadow-gray-500 rounded-lg hover:scale-105 ease-in duration-100">
-              <Image src="/../public/assets/skills/express.png" alt="Express logo" width={100} height={50} />
+            {/* <div className="flex justify-between items-center p-4 shadow-lg shadow-gray-500 rounded-lg hover:scale-105 ease-in duration-100">
+              <Image src={ExpressJSLogo} alt="Express logo" width={100} height={50} />
               <p>ExpressJS</p>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
