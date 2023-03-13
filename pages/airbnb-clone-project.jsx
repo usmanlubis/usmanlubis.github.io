33000d39d1d0cf7ab19724aa9a32df150b4c4770
usmanlubis <usmanlubis11@gmail.com>
2023-03-13 10:54:56 +0700

    add import head

--- pages/airbnb-clone-project.jsx
+++ pages/airbnb-clone-project.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head';
 import Image from 'next/image';
 import AirBnbClone from '../public/assets/projects/airbnb-clone.png';
 import { RiRadioButtonFill } from 'react-icons/ri';
