2bebc92928552bc6ba43700483853c8847129be7
Usman Lubis <usmanlubis11@gmail.com>
2024-03-18 21:04:32 +0700

    update skills section

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import Image from 'next/image';
-import HtmlLogo from '../public/assets/skills/html.png';
-import CssLogo from '../public/assets/skills/css.png';
-import JavascriptLogo from '../public/assets/skills/javascript.png';
+import TypeScriptLogo from '../public/assets/skills/typescript.png';
 import ReactJSLogo from '../public/assets/skills/react.png';
 import TailwindLogo from '../public/assets/skills/tailwind.png';
 import NextJSLogo from '../public/assets/skills/nextjs.png';
-import GitLogo from '../public/assets/skills/git.png';
 import NodeJSLogo from '../public/assets/skills/node.png';
-// import ExpressJSLogo from '../public/assets/skills/express.png';
+import PHPLogo from '../public/assets/skills/php.png';
+import LaravelLogo from '../public/assets/skills/laravel.png';
+import GitLogo from '../public/assets/skills/git.png';
 
 function Skills() {
   return (
@@ -18,16 +17,8 @@ function Skills() {
           <h2 className="mb-8">What I can do</h2>
           <div className="grid md:grid-cols-2 lg:grid-cols-4 w-full gap-4 skills">
             <div className="flex justify-between items-center p-4 shadow-lg shadow-gray-500 rounded-lg hover:scale-105 ease-in duration-100">
-              <Image src={HtmlLogo} alt="Html logo" width={50} height={50} />
-              <p>HTML</p>
-            </div>
-            <div className="flex justify-between items-center p-4 shadow-lg shadow-gray-500 rounded-lg hover:scale-105 ease-in duration-100">
-              <Image src={CssLogo} alt="Css logo" width={50} height={50} />
-              <p>CSS</p>
-            </div>
-            <div className="flex justify-between items-center p-4 shadow-lg shadow-gray-500 rounded-lg hover:scale-105 ease-in duration-100">
-              <Image src={JavascriptLogo} alt="JavaScript logo" width={50} height={50} />
-              <p>JavaScript</p>
+              <Image src={TypeScriptLogo} alt="Typescript Logo" width={50} height={50} />
+              <p>TypeScript</p>
             </div>
             <div className="flex justify-between items-center p-4 shadow-lg shadow-gray-500 rounded-lg hover:scale-105 ease-in duration-100">
               <Image src={ReactJSLogo} alt="React JS logo" width={50} height={50} />
@@ -44,6 +35,14 @@ function Skills() {
             <div className="flex justify-between items-center p-4 shadow-lg shadow-gray-500 rounded-lg hover:scale-105 ease-in duration-100">
               <Image src={GitLogo} alt="Git logo" width={50} height={50} />
               <p>Git</p>
+            </div>
+            <div className="flex justify-between items-center p-4 shadow-lg shadow-gray-500 rounded-lg hover:scale-105 ease-in duration-100">
+              <Image src={PHPLogo} alt="PHP logo" width={50} height={50} />
+              <p>PHP</p>
+            </div>
+            <div className="flex justify-between items-center p-4 shadow-lg shadow-gray-500 rounded-lg hover:scale-105 ease-in duration-100">
+              <Image src={LaravelLogo} alt="Laravel logo" width={50} height={50} />
+              <p>Laravel</p>
             </div>
             <div className="flex justify-between items-center p-4 shadow-lg shadow-gray-500 rounded-lg hover:scale-105 ease-in duration-100">
               <Image src={NodeJSLogo} alt="Node logo" width={50} height={50} />
--- pages/simple-app-project.jsx
+++ pages/simple-app-project.jsx
@@ -10,7 +10,7 @@ function SimpleNotesApp() {
   return (
     <>
       <Head>
-        <title>AirBnb Clone - Usman Lubis</title>
+        <title>Simple Notes App - Usman Lubis</title>
         <meta name="description" content="Air Bnb Clone project by Usman Lubis" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/assets/Favicon.png" />
@@ -27,7 +27,7 @@ function SimpleNotesApp() {
             alt="/"
           />
           <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
-            <h2 className="py-2">AirBnb Clone</h2>
+            <h2 className="py-2">Notes App</h2>
             <h3>React JS | CSS</h3>
           </div>
         </div>
